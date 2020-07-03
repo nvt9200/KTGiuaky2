a1177b16eb318dd2ce1f4c81c2a3c919a29d0306
= <=>
2020-07-03 22:33:26 +0700

    first commit

---
+++ WebMVC/WebMVC/Models/mvcPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class mvcPhone
+    {
+
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string GeneralNote { get; set; }
+
+    }
+}
